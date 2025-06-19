7805a1e08d444d28337a2b4d4a8cb4cab9d67832
kiran-91 <deshpandekiran007kd@gmail.com>
2025-06-19 14:53:09 +0530

    adding sql files

---
+++ select_statement.sql
@@ -0,0 +1,14 @@
+select *
+from parks_and_recreation.employee_demographics;
+
+select * 
+from parks_and_recreation.employee_salary;
+
+select first_name, last_name, occupation
+from parks_and_recreation.employee_salary;
+
+select first_name, last_name, age, age+10
+from parks_and_recreation.employee_demographics;
+
+select first_name, last_name, age, age+10 as age_plus_10
+from parks_and_recreation.employee_demographics;

316e31e2230bdea08bac027474379a47d8e874b3
kiran-91 <deshpandekiran007kd@gmail.com>
2025-06-19 15:36:45 +0530

    added queries to where clause

--- where_clause.sql
+++ where_clause.sql
@@ -26,8 +26,25 @@ select *
 from parks_and_recreation.employee_demographics
 where  birth_date < "1980-01-01";
 
--- AND clause--
+-- AND OR NOT clause--
 
 select * 
 from parks_and_recreation.employee_demographics
 where  birth_date > "1980-01-01" and gender="Male";
+
+select * 
+from parks_and_recreation.employee_demographics
+where  birth_date > "1980-01-01" OR gender="Male";
+
+select * 
+from parks_and_recreation.employee_demographics
+where  birth_date > "1980-01-01" OR NOT gender="Male";
+
+-- like clause --
+select *
+from parks_and_recreation.employee_demographics
+where first_name like "Jer%";
+
+select *
+from parks_and_recreation.employee_demographics
+where first_name like "%a%"
